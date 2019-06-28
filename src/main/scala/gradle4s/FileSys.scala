113b6efc96941f03144ffac607177ddde7635cdd
lombardo-chcg <dpa.lombardo@gmail.com>
2019-06-28 00:16:52 -0500

    add basic template and template transformer

--- src/main/scala/gradle4s/FileSys.scala
+++ src/main/scala/gradle4s/FileSys.scala
@@ -5,7 +5,7 @@ import ammonite.ops._
 import zio.{IO, Task}
 
 object FileSys {
-
+  private val templateDir = pwd/"sample-template"
   private val parentDir: ops.Path = pwd/up
 
   def defaultPath: ops.Path = parentDir
@@ -13,4 +13,6 @@ object FileSys {
   def toPath(s: String): Task[ops.Path] = IO.effect(Path(s))
 
   def fileExists(req: ops.Path): Task[Boolean] = IO.effect(exists(req))
+
+  def cpTemplate(path: os.Path): Task[Unit] = IO.effect(cp(templateDir, path))
 }
--- src/main/scala/gradle4s/Main.scala
+++ src/main/scala/gradle4s/Main.scala
@@ -5,9 +5,12 @@ import zio._
 import zio.console._
 
 object Main extends App {
+  val program = for {
+    u  <- getUserRequest
+    _  <- copyTemplate(u)
+  } yield u
 
-  def run(args: List[String]) =
-    getUserRequest.fold(
+  def run(args: List[String]) = program.fold(
       // exit code 0 for all because gradle cli hijacks error code results and displays noisy output
       {
         case th: AppError => println(th.getMessage); 0
@@ -16,34 +19,28 @@ object Main extends App {
       _ => { println("Success"); 0 }
     )
 
-  val getUserRequest: ZIO[Console, Throwable, UserRequest] =
+  def getUserRequest: ZIO[Console, Throwable, UserRequest] =
     for {
-      _           <- putStrLn("🐘🚀 Gradle + Scala Project Starter 🐘🚀")
-      projectName <- promptUser("Enter project name: ")
+      _           <- putStrLn("🐘💃 gradle4s 💃🐘")
+      projectName <- ConsoleOps.promptUser("Enter project name: ")
       pn          <- Rules.validProjectName(projectName)
 
-      packageName <- promptUser(s"Enter package name (${Rules.defaultPkg}): ", Rules.defaultPkg, UIO.succeed(_))
+      packageName <- ConsoleOps.promptUser(s"Enter package name (${Rules.defaultPkg}): ", Rules.defaultPkg, UIO.succeed(_))
       pkg         <- Rules.validatePackageName(packageName)
 
       pth   = FileSys.defaultPath
-      path  <- promptUser(s"Enter project path ($pth/$pn): ", pth, FileSys.toPath _)
+      path  <- ConsoleOps.promptUser(s"Enter project path ($pth/$pn): ", pth, FileSys.toPath _)
 
       ur = UserRequest(pn, pkg, path)
       b  <- FileSys.fileExists(ur.path)
       _  <-  if (!b) ZIO.succeed(b) else ZIO.fail(FileExistsError(ur))
     } yield ur
 
-  private def promptUser[T](prompt: String, default: T, transformer: String => Task[T]): ZIO[Console, Throwable, T] =
-    putStr(prompt)
-      .flatMap(_ => getStrLn)
-      .flatMap(s => {
-        if (s.isEmpty) UIO.succeed(default) else transformer(s)
-      })
-
-  private def promptUser(prompt: String): ZIO[Console, Throwable, String] =
-    putStr(prompt)
-      .flatMap(_ => getStrLn)
-      .flatMap(s => {
-        if (s.isEmpty) promptUser(prompt) else IO.succeed(s)
-      })
+  def copyTemplate(u: UserRequest): ZIO[Console, Throwable, UserRequest] =
+    for {
+      _   <- putStrLn(s"creating new Scala Gradle project at (${u.path})")
+      _   <- FileSys.cpTemplate(u.path)
+      _   <- TemplateOps.transform(u)
+      _   <- if (u.packageName != Rules.defaultPkg) TemplateOps.moveSrcFiles(u) else IO.succeed(u)
+    } yield u
 }
